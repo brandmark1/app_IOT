86c9d806cb8d12569e1603fdd06cf0df61d6f199
brandmark1 <74679136+brandmark1@users.noreply.github.com>
2023-09-07 16:47:08 -0600

    Unidad1 y Unidad2

---
+++ Unidad2/serverwifi.ino
@@ -0,0 +1,17 @@
+#include <WiFi.h>
+
+const char *ssid = "Wifa";
+const char *password = "pass";
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("\n[*] Creating AP");
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+  Serial.print("[+] AP Created with IP Gateway ");
+  Serial.println(WiFi.softAPIP());
+}
+
+void loop() {}
+
